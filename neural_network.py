175b656a15ac7d77eb8075f6f26a13ab5b0ae89b
Vincent Do <wavyfingers@gmail.com>
2023-12-26 16:40:31 -0600

    new update

--- neural_network.py
+++ neural_network.py
@@ -1,218 +1,117 @@
+import pandas as pd
 import chess.pgn
+import chess
+import io
 import re
-import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler
+from tensorflow.keras.layers import Input, Dense, Conv2D, Flatten, LSTM
+from tensorflow.keras.models import Model
+import numpy as np
 
-# Define the number of unique pieces and positions on the board
-num_pieces = 6  # Pawn, Knight, Bishop, Rook, Queen, King
-num_positions = 64  # 8x8 board
+piece_to_index = {
+    'p': 0, 'n': 1, 'b': 2, 'r': 3, 'q': 4, 'k': 5,
+    'P': 6, 'N': 7, 'B': 8, 'R': 9, 'Q': 10, 'K': 11
+}
 
-# Regular expression pattern to extract evaluation and best move
-eval_pattern = re.compile(r"[%]eval:\s+([-+]?\d+)")
-best_move_pattern = re.compile(r"[%]best_move:\s+(\S+)")
+def fen_to_tensor(fen):
+    # Initialize an empty tensor
+    tensor = np.zeros((8, 8, 14), dtype=np.float32)
+    
+    # Split the FEN string to get the piece placement
+    pieces = fen.split(' ')[0]
+    rows = pieces.split('/')
+    
+    for i, row in enumerate(rows):
+        j = 0
+        for char in row:
+            if char.isdigit():
+                # Empty squares
+                j += int(char)
+            else:
+                # Set the corresponding layer for the piece
+                tensor[i, j, piece_to_index[char]] = 1
+                j += 1
+    return tensor
 
-# Load PGN data from a single file
-train_pgn_file = "c:/Users/vince/Downloads/magnus_evalv2.pgn"
+def square_to_index(square):
+    # Converts a square in algebraic notation to an index (0-63)
+    rank = 8 - int(square[1]) # Rank (row)
+    file = ord(square[0]) - ord('a') # File (column)
+    return 8 * rank + file
 
-# Load PGN data using python-chess
-pgn = open(train_pgn_file)
-games = []
-max_moves = 0
-while True:
-    game = chess.pgn.read_game(pgn)
-    if game is None:
-        break
+def move_to_vector(move):
+    # Converts a chess move to a one-hot encoded vector
+    vector = np.zeros(128, dtype=np.float32)
+    from_square = square_to_index(str(move)[:2])
+    to_square = square_to_index(str(move)[2:])
+    vector[from_square] = 1
+    vector[64 + to_square] = 1
+    return vector
 
-    # Count the number of moves in the game
-    num_moves = sum(1 for _ in game.mainline_moves())
 
-    # TODO update max moves
-    # Update max_moves if necessary
-    if num_moves > max_moves:
-        max_moves = num_moves
+# Parsing the PGN file to extract relevant data
+def parse_pgn(file_path):
+    with open(file_path) as pgn_file:
+        while True:
+            game = chess.pgn.read_game(pgn_file)
+            if game is None:
+                break
 
-    games.append(game)
+            board = game.board()
+            for node in game.mainline():
+                move = node.move
+                fen = board.fen()
+                board_tensor = fen_to_tensor(fen)
+                move_vector = move_to_vector(move)
 
-pgn.close()
-print(max_moves)
+                # Extracting additional information from comments
+                comment = node.comment
+                eval_match = re.search(r'\[%eval: ([^\]]+)\]', comment)
+                best_move_match = re.search(r'\[%best_move: ([^\]]+)\]', comment)
+                played_best_move_match = re.search(r'\[%played_best_move: ([^\]]+)\]', comment)
 
-def move_to_numeric(move):
-    # Convert algebraic notation to a one-hot encoded array
+                evaluation = eval_match.group(1) if eval_match else None
+                best_move = best_move_match.group(1) if best_move_match else None
+                played_best_move = played_best_move_match.group(1) if played_best_move_match else None
 
-    # Define the alphabet for the chess squares (a-h)
-    alphabet = 'abcdefgh'
+                # Yielding the data for each move
+                yield board_tensor, move_vector, evaluation, best_move, played_best_move
+                board.push(move)
 
-    # Get source and target squares from the move object
-    source_square = move.from_square
-    target_square = move.to_square
+def create_chess_model():
+    # Board input: 8x8x14 tensor
+    board_input = Input(shape=(8, 8, 14), name='board_input')
 
-    # Create a one-hot encoded array for the move
-    move_numeric = [0] * (num_positions * 2)  # Two positions for source and target squares
-    move_numeric[source_square] = 1
-    move_numeric[num_positions + target_square] = 1
+    # CNN layers
+    x = Conv2D(64, kernel_size=(3, 3), activation='relu')(board_input)
+    x = Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
+    x = Flatten()(x)
 
-    return move_numeric
+    # Move sequence input: This should be a sequence of move vectors
+    # Adjust the input shape as per your move encoding
+    move_input = Input(shape=(None, 128), name='move_input')
 
-# Process training data and prepare inputs and ground truth
-x_train, y_train_eval, y_train_move = [], [], []
+    # RNN layer
+    y = LSTM(64)(move_input)
 
-for game in games:
-    board = game.board()
-    for move_number, move in enumerate(game.mainline_moves()):
-        fen = board.fen()
+    # Combine CNN and RNN outputs
+    combined = tf.keras.layers.concatenate([x, y])
 
-        # Extract evaluation from PGN data
-        eval_match = eval_pattern.search(str(move))
-        if eval_match:
-            evaluation = int(eval_match.group(1))
-        else:
-            evaluation = 0  # Default evaluation if not found
+    # Dense layers for decision making
+    z = Dense(128, activation='relu')(combined)
+    z = Dense(64, activation='relu')(z)
 
-        # Extract best move from PGN data
-        best_move_match = best_move_pattern.search(str(move))
-        if best_move_match:
-            best_move = best_move_match.group(1)
-        else:
-            best_move = None  # Default best move if not found
+    # Output layer: Adjust the number of units to match the size of your move encoding
+    move_output = Dense(128, activation='softmax', name='move_output')(z)
 
-        # Append data to lists
-        x_train.append(move_to_numeric(move))
-        y_train_eval.append(evaluation / 100.0)  # Normalize evaluation to [-1, 1]
-        y_train_move.append(1.0 if best_move else 0.0)
+    model = Model(inputs=[board_input, move_input], outputs=[move_output])
 
-# Convert lists to NumPy arrays
-x_train = np.array(x_train)
-y_train_eval = np.array(y_train_eval)
-y_train_move = np.array(y_train_move)
-
-# Create a feedforward neural network model
-def create_model(input_shape):
-    model = keras.Sequential([
-        layers.Input(shape=input_shape),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(64, activation='relu'),
-        layers.Dense(32, activation='relu'),
-        layers.Dense(1, activation='linear'),  # Output layer for evaluation prediction
-        layers.Dense(1, activation='sigmoid')  # Output layer for best move prediction
-    ])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-# Custom loss function for chess move prediction
-def custom_chess_loss(y_true, y_pred):
-    # Split y_pred into evaluation predictions and best move predictions
-    eval_pred = y_pred[:, 0]
-    move_pred = y_pred[:, 1]
+# Use this function to parse your PGN file
+for board_tensor, move_vector, evaluation, best_move, played_best_move in parse_pgn('C:/Users/vince/Downloads/games.pgn'):
+    print(board_tensor, move_vector, evaluation, best_move, played_best_move)
 
-    # Split y_true into actual evaluations and best move indicators
-    actual_eval = y_true[:, 0]
-    best_move_indicators = y_true[:, 1]
-
-    # Calculate mean squared error for evaluation predictions
-    mse_eval = keras.losses.mean_squared_error(actual_eval, eval_pred)
-
-    # Calculate binary cross-entropy for best move predictions
-    bce_move = keras.losses.binary_crossentropy(best_move_indicators, move_pred)
-
-    # Combine the two loss components with appropriate weights
-    total_loss = mse_eval + bce_move
-
-    return total_loss
-
-# Define input shape based on the number of features in your input data
-num_moves = max_moves
-input_shape = (num_moves * (num_pieces + num_positions) + 1,)
-
-# Create and compile the model
-model = create_model(input_shape)
-model.compile(optimizer='adam', loss=custom_chess_loss, metrics=['mae'])
-
-# Define callbacks for early stopping and learning rate scheduling
-early_stopping = EarlyStopping(patience=10, restore_best_weights=True)
-def lr_scheduler(epoch, lr):
-    if epoch < 30:
-        return lr
-    else:
-        return lr * tf.math.exp(-0.1)
-learning_rate_scheduler = LearningRateScheduler(lr_scheduler)
-
-# Train the model with callbacks
-batch_size = 32
-num_epochs = 50
-history = model.fit(
-    x_train, [y_train_eval, y_train_move], batch_size=batch_size, epochs=num_epochs,
-    validation_split=0.2, callbacks=[early_stopping, learning_rate_scheduler]
-)
-
-# Save the trained model
-model.save('trained_chess_model.h5')
-
-# -----------------------
-# Testing the trained model
-
-# Load the trained model
-model = keras.models.load_model('trained_chess_model.h5')
-
-# Load PGN data from your test PGN file
-test_pgn_file = "c:/Users/vince/Downloads/GM_games_eval.pgn.pgn"
-
-# Load PGN data using python-chess
-pgn = open(test_pgn_file)
-games = []
-
-while True:
-    game = chess.pgn.read_game(pgn)
-    if game is None:
-        break
-    games.append(game)
-
-pgn.close()
-
-# Process test data and prepare inputs and ground truth
-x_test, y_test_eval, y_test_move = [], [], []
-
-for game in games:
-    board = game.board()
-    for move_number, move in enumerate(game.mainline_moves()):
-        fen = board.fen()
-
-        # Extract evaluation from PGN data
-        eval_match = eval_pattern.search(str(move))
-        if eval_match:
-            evaluation = int(eval_match.group(1))
-        else:
-            evaluation = 0  # Default evaluation if not found
-
-        # Extract best move from PGN data
-        best_move_match = best_move_pattern.search(str(move))
-        if best_move_match:
-            best_move = best_move_match.group(1)
-        else:
-            best_move = None  # Default best move if not found
-
-        # Append data to lists
-        x_test.append(move_to_numeric(move))
-        y_test_eval.append(evaluation / 100.0)  # Normalize evaluation to [-1, 1]
-        y_test_move.append(1.0 if best_move else 0.0)
-
-# Convert lists to NumPy arrays
-x_test = np.array(x_test)
-y_test_eval = np.array(y_test_eval)
-y_test_move = np.array(y_test_move)
-
-# Predict using the model
-predictions = model.predict(x_test)
-
-# Extract predicted evaluation scores and best move indicators
-predicted_evaluations = predictions[:, 0]
-predicted_best_moves = (predictions[:, 1] > 0.5).astype(int)
-
-# Evaluate model performance
-eval_mse = np.mean((y_test_eval - predicted_evaluations)**2)
-move_accuracy = np.mean(np.equal(y_test_move, predicted_best_moves))
-
-# Display results
-print("Evaluation MSE:", eval_mse)
-print("Move Accuracy:", move_accuracy)
+chess_model = create_chess_model()
+chess_model.summary()
